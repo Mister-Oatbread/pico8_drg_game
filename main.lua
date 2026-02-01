bdc7553086854bd825e22bb302113e710ae67854
Mister-Oatbread <david.hafe82@gmail.com>
2026-02-01 23:21:58 +0100

    added floor generation

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
 
 
 function _init()
+    initialize_map()
 end
 
 function _update60()
 end
 
 function _draw()
+    cls();
+    camera(101,101);
+    draw_map();
 end
 
 
--- map.lua
+++ map.lua
@@ -2,11 +2,32 @@
 
 -- this file contains the needed files to generate the rolling background
 
+-- takes x,y coord and creates a random base sprite with a 70% chance,
+-- or special sprite else
+function _produce_map_entity(x_coord, y_coord)
+    if (rnd(1) < .75) then
+        sprite_list = map_sprites_base;
+    else
+        sprite_list = map_sprites_special;
+    end
+    local x_flip = rnd(2) < 1;
+    local y_flip = rnd(2) < 1;
+
+    local sprite = sprite_list[flr(rnd(#sprite_list))+1];
+    return {
+        sprite=sprite,
+        x_coord=x_coord,
+        y_coord=y_coord,
+        x_flip=x_flip,
+        y_flip=y_flip,
+    };
+end
+
 -- initialize map
 function initialize_map()
     -- catalogue all sprites and infrastructure
-    map_entity = {spr, x_coord, y_coord};
-    wall_entity = {spr,x_coord, y_coord};
+    map_entity = {sprite, x_coord, y_coord};
+    wall_entity = {sprite,x_coord, y_coord};
     map_sprites_base = {128,144,160,176};
     map_sprites_special = {
         130,146,162,178,
@@ -23,21 +44,33 @@ function initialize_map()
     camera_position = {x_coord=101, y_coord = 101};
 
     -- initialize terrain by looping over all positions and generating
-    x_coord = 101;
-    y_coord = 91;
+    local x_coord = 101;
+    local y_coord = 91;
     while (x_coord < 228) do
         while (y_coord < 228) do
-            spr =
-            terrain.add(_produce_map_entity(spr,x_coord,y_coord);
-            x_coord += 8;
-            y_coord += 8;
+            add(terrain,_produce_map_entity(x_coord,y_coord));
+            y_coord = y_coord + 8;
         end
+        x_coord = x_coord + 8;
+        y_coord = 91;
     end
-
 end
 
-function _produce_map_entity(spr, x_coord, y_coord)
-    return {spr=spr, x_coord=x_coord, y_coord=y_coord};
+-- paint all terrain tiles at their current location
+function draw_map()
+    local sprite;
+    local x_coord;
+    local y_coord;
+    local x_flip;
+    local y_flip;
+    for i=1,#terrain do
+        sprite = terrain[i].sprite;
+        x_coord = terrain[i].x_coord;
+        y_coord = terrain[i].y_coord;
+        x_flip = terrain[i].x_flip;
+        y_flip = terrain[i].y_flip;
+        spr(sprite,x_coord,y_coord,1,1,x_flip,y_flip);
+    end
 end
 
 
