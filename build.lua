3596dee6a95d9abff4416516c79e9150c465be62
Mister-Oatbread <david.hafe82@gmail.com>
2026-05-09 18:50:09 +0200

    removed old files, started player rewrite

--- build.lua
+++ build.lua
@@ -1161,106 +1161,93 @@ reset_cpu_load=function() cpu_percentage=0 end,
 }
 end
 function new_player(number,role)
+local function change_role(new_role)
+role=new_role
+max_ammo=role=="gunner" and 100 or 25
+ammo=max_ammo
+max_fuel=role=="driller" and 250 or 0
+fuel=max_fuel
+shot_delay=role=="gunner" and 1 or 3
+dset(10+number,role_numbers[role])
+end
 local x=140+8*number
 local y=205
 local frame=0
+local role_numbers={driller=1,gunner=2,engineer=3}
 local number=number
 local role=role
-local is={
-moving={up,down,left,right},
-shooting=false,
-drilling=false,
-mining=false,
-rns=false}
-local was_mining=false
-local drills_damage=4
-local mining_damage=10
-local playing_sound_of={drill=false,gun=false}
-local points=0
-local max_ammo=role=="gunner" and 100 or 25
-local ammo=max_ammo
-local max_fuel=role=="driller" and 250 or 0
-local fuel=max_fuel
-local health=3
-local max_health=3
-local mining_since=60
-local mining_delay=10
-local hit_since=60
-local shot_since=60
+local is_walking=false
+local is_moving_right=false
+local is_moving_left=false
+local is_sprinting=false
+local is_standing=true
+local is_crawling=false
+local is_mining=false
+local is_drilling=false
+local is_shooting=false
+local mining_since=30
+local mining_delay=3
+local shot_since=30
 local shot_delay=role=="gunner" and 1 or 3
-local collision_points={left={},right={},top={}}
-local has_collision={left=false,right=false,top=false}
-for i=1,8 do
-add(collision_points.left,{x=0,y=0})
-add(collision_points.right,{x=0,y=0})
-add(collision_points.top,{x=0,y=0})
+local max_ammo,max_fuel,fuel,ammo
+change_role(role)
+local max_health=3
+local health=max_health
+local hit_since=60
+local collision_points_left={}
+local collision_points_right={}
+local collision_points_top={}
+local has_collision_left=false
+local has_collision_right=false
+local has_collision_top=false
+for i=1,6 do
+add(collision_points_left,{x=0,y=0})
+add(collision_points_right,{x=0,y=0})
+add(collision_points_top,{x=0,y=0})
 end
 local function fetch_inputs()
 local p=number-1
-is.shooting=btn(5,p) and not btn(4,p)
-if role=="gunner" and is.shooting then
-is.moving.up=false
-is.moving.left=false
-is.moving.right=false
-is.moving.down=playing
-else
-is.moving.up=btn(2,p)
-is.moving.down=btn(3,p)
-is.moving.left=btn(0,p)
-is.moving.right=btn(1,p)
-end
-is.rns=btn(3,p) and btn(4,p) and btn(5,p)
-if btn(4,p) and not btn(5,p) then
-is.drilling=fuel>0
-is.mining=not is.drilling
-else
-is.drilling=false
-is.mining=false
-end
+is_moving_left=btn(0,p)
+is_moving_right=btn(1,p)
+is_standing=(
+playing and btn(3,p)
+) or (
+at_title_screen and not (btn(3,p) or btn(2,p))
+)
+is_sprinting=btn(2,p) and playing
+is_drilling=btn(4,p) and not btn(5,p) and fuel>0
+is_mining=btn(4,p) and not btn(5,p) and fuel=0
+is_shooting=btn(5,p) and not btn(4,p)
+is_crawling=role=="gunner" and is_shooting
 end
 local function update_player_collision_points()
 for i=1,6 do
-collision_points.left[i].x=x
-collision_points.left[i].y=y+i
+collision_points_left[i].x=x
+collision_points_left[i].y=y+i
 end
 for i=1,6 do
-collision_points.right[i].x=x+7
-collision_points.right[i].y=y+i
+collision_points_right[i].x=x+7
+collision_points_right[i].y=y+i
 end
 for i=1,6 do
-collision_points.top[i].x=x+i
-collision_points.top[i].y=y-1
+collision_points_top[i].x=x+i
+collision_points_top[i].y=y-1
 end
 end
 local function find_terrain_collision()
-local point,color
-has_collision.left=false
-has_collision.right=false
-has_collision.top=false
-for i=1,#collision_points.left do
-point=collision_points.left[i]
-color=pget(point.x,point.y)
+local function check_all(points)
+local color
+for i=1,#points do
+color=pget(points[i].x, points[i].y)
 if color==5 or color==13 then
-has_collision.left=true
-break
+return true
 end
 end
-for i=1,#collision_points.right do
-point=collision_points.right[i]
-color=pget(point.x,point.y)
-if color==5 or color==13 then
-has_collision.right=true
-break
+return false
 end
-end
-for i=1,#collision_points.top do
-point=collision_points.top[i]
-color=pget(point.x,point.y)
-if color==5 or color==13 then
-has_collision.top=true
-break
-end
-end
+has_collision_left=check_all(collision_points_left)
+has_collision_right=check_all(collision_points_right)
+has_collision_top=check_all(collision_points_top)
 end
 local function mine()
 if mining_since>mining_delay then
@@ -1268,17 +1255,6 @@ map.add_drilled_ground(53,x,y-2)
 sfx(-1,number)
 sfx(31,number)
 mining_since=0
-end
-end
-local function drill()
-if fuel>0 then
-map.add_drilled_ground(52,x,y-2)
-fuel-=1
-if not playing_sound_of.drill then
-sfx(-1,number)
-sfx(30,number)
-playing_sound_of.drill=true
-end
 end
 end
 local function shoot()
@@ -1298,58 +1274,20 @@ end
 shot_since=0
 end
 end
-local function give_ammo(percentage)
+local function drill()
+if fuel>0 then
+map.add_drilled_ground(52,x,y-2)
+fuel-=1
+if not playing_sound_of.drill then
+sfx(-1,number)
+sfx(30,number)
+playing_sound_of.drill=true
+end
+end
+end
+local function give_resources(percentage)
 ammo=min(ammo+max_ammo*percentage,max_ammo)
 fuel=min(fuel+max_fuel*percentage,max_fuel)
-end
-local function move_player()
-if is.moving.up and not has_collision.top then
-if y>102 then
-y-=1
-end
-end
-if is.moving.down then
-if y<221 then
-y+=1
-end
-end
-if is.moving.left and not has_collision.left then
-if x>=102 then
-x-=1
-end
-end
-if is.moving.right and not has_collision.right then
-if x<=220 then
-x+=1
-end
-end
-if has_collision.top and playing then
-y+=1
-end
-if y<=102 then y=102 end
-if y>=221 then y=221 end
-end
-local function update()
-fetch_inputs()
-update_player_collision_points()
-find_terrain_collision()
-move_player()
-if is.drilling then
-drill()
-elseif playing_sound_of.drill then
-sfx(-1,number)
-playing_sound_of.drill=false
-end
-if is.mining then mine() end
-if is.shooting then
-shoot()
-elseif playing_sound_of.gun then
-sfx(-1,number)
-playing_sound_of.gun=false
-end
-mining_since=min(mining_since+1,1000)
-shot_since=min(shot_since+1,1000)
-hit_since=min(hit_since+1,1000)
 end
 local function damage_player(amount)
 if hit_since>30 then
@@ -1358,96 +1296,24 @@ sfx(32,number)
 hit_since=0
 end
 end
-local function draw_gun()
-pset(x+6,y,6)
-pset(x+6,y+1,6)
-if role=="gunner" then
-pset(x+5,y,6)
-pset(x+7,y,6)
-pset(x+7,y+1,6)
-pset(x+7,y+2,6)
-pset(x+7,y+3,6)
-pset(x+7,y+4,6)
-end
-end
-local function draw_drills()
-pset(x+6,y,5)
-pset(x+6,y+1,5)
-pset(x+5,y,5)
-pset(x+2,y,5)
-pset(x+1,y,5)
-pset(x+1,y+1,5)
-end
-local function draw_pickaxe()
-pset(x+6,y,5)
-pset(x+6,y+1,4)
-pset(x+6,y+2,4)
-end
-local function change_role(new_role)
-role=new_role
-max_ammo=role=="gunner" and 100 or 25
-ammo=max_ammo
-max_fuel=role=="driller" and 250 or 0
-fuel=max_fuel
-shot_delay=role=="gunner" and 1 or 3
-local role_ids={driller=1,gunner=2,engineer=3}
-dset(10+number,role_ids[role])
+local function update()
+fetch_inputs()
+update_player_collision_points()
+find_terrain_collision()
+if is_drilling then drill() end
+if is_shooting then shoot() end
+if is_mining then mine() end
+shot_since+=1
+mining_since+=1
 end
 local function draw()
-local moving,x_flip
-local speed=1
-if playing then
-moving=(not is.moving.down
-or is.moving.left or is.moving.right)
-if playing and is.moving.up then speed=2 end
-elseif at_title_screen then
-moving=(is.moving.down or is.moving.up
-or is.moving.left or is.moving.right)
-end
-local sprite=role=="driller" and 48 or 32
-if role=="engineer" then sprite=34 end
-if moving then sprite+=1 end
-x_flip=frame>=8
-frame=(frame+speed)%16
-if hit_since<=30 then
-pal(10,2)
-pal(3,2)
-pal(8,2)
-end
-spr(sprite,x,y,1,1,x_flip,false)
-pal()
-if is.shooting then draw_gun() end
-if is.drilling then draw_drills() end
-if mining_since<mining_delay*.7 then draw_pickaxe() end
 end
 return {
 update=update,
 draw=draw,
-damage=damage_player,
-give_ammo=give_ammo,
 change_role=change_role,
-give_points=function(amount) points+=amount end,
-give_health=function(amount) health=min(health+amount,max_health) end,
-get_hitbox=function() return {x={x+1,x+6},y={y,y+7}} end,
-get_mining_hitbox=function() return {x={x,x+7},y={y-1,y+3}} end,
-get_damaging_hitbox=function() return {x={x,x+7},y={y-3,y+3}} end,
-reposition=function(x_new, y_new) x=x_new;y=y_new end,
-x=function() return x end,
-y=function() return y end,
-is_drilling=function() return is.drilling end,
-is_shooting=function() return is.shooting end,
-is_mining=function() return mining_since<2 end,
-get_role=function() return role end,
-is_rns=function() return is.rns end,
-health=function() return health end,
-number=function() return number end,
-ammo=function() return ammo end,
-fuel=function() return fuel end,
-points=function() return points end,
-max_ammo=function() return max_ammo end,
-max_fuel=function() return max_fuel end,
-drills_damage=drills_damage,
-mining_damage=mining_damage,
+give_resources=give_resources,
+damage_player=damage_player,
 }
 end
 function new_projectiles()
